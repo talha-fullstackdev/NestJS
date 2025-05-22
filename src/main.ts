f8d05b6ce3a698cdd9929c432581253184edba67
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-22 12:20:12 +0500

    write a console msg of local host server

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log("app is runing at http://localhost:3000/") // write 
 }
 bootstrap();
