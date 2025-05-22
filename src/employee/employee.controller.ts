ed7a03f86afc5e0fc9a0985f1fc6f31c27d82461
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-22 16:22:32 +0500

    get response from employee route (e.g employee module or employee controller

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+    @Get()
+    getEmployee(){
+        return "hello talha from employee module"
+    }
+}
