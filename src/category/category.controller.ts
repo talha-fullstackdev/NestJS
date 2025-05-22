5de69f5770af1fda495405a45e4b2cb9efe02777
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-22 18:40:56 +0500

    added category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor(private readonly categoryService:CategoryService){} // injecting CategoryService here likr this  
+    @Get()
+    getAllCategories(){
+      return this.categoryService.getCategory()
+   }
+}
