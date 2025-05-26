148c5f6639cdc93cc733bb9e66eee9475ea21af2
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-26 18:52:58 +0500

    implemmnt getallstudent and getonestudent route

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,11 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 import { StudentService } from './student.service';
 @Controller('student')
 export class StudentController {
     constructor(private readonly studentService:StudentService){}
     @Get()
-    getStudent(){
-    return this.studentService.getStudent()
+    getAllStudents(){
+        return this.studentService.getALLStudents()
+    }
+    @Get(":id") // id route
+    getOne(@Param("id") id:string){ // passing id like this here 
+        return this.studentService.getStudentById(Number(id))
     }
 }
