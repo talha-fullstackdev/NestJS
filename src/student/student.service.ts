de3599617afb9507e20fffaf5576b35e858aae10
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-26 17:44:09 +0500

    made a student detials array in services

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -3,7 +3,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StudentService {
+    private students =[
+        {rollnumber:101,name:"Talha Nawaz",age:25},
+        {rollnumber:102,name:"Uzair Ansari",age:24},
+        {rollnumber:103,name:"Daud Ahmad",age:23},
+        {rollnumber:104,name:"Shameel Majeed",age:22},
+        {rollnumber:105,name:"Usman Liaquat",age:24},
+    ]
     getStudent(){
-        return "Talha nawaz"
+        return this.students
     }
 }
