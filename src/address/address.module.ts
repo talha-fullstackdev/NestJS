cf61becc42895b8a2a531c284006d5a12996ea1c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-26 16:12:50 +0500

    add address module

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
