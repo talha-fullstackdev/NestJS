131a8179d1c8f68e2b00a1088b768fcb6195fcf2
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-26 14:58:32 +0500

    working with address module

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Controller ,Get} from '@nestjs/common';
-
+import { AddressService } from './address.service';
 @Controller('address')
 export class AddressController {
+    constructor(private readonly addressService :AddressService){} //automatically inject AddressService dependancy here . we also have to provide empty brackets like these in the constructor
     @Get()
-    getResponse(){
-        return "address page"
+    getAddress(){
+        return this.addressService.getAddress()
     }
 }
--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AddressService {}
+
+export class AddressService {
+  
+    getAddress(){
+        const addressDetails = {
+        id:1001,
+        name:"Talha Nawaz",
+        age:25,
+        gender:"male",
+        contact:"03480951759",
+        address:{
+            current_residence :"House #2 street 7 madina colony islamnagr walton road opposite toyota cant motors",
+            permanent:"house # 878 toheed colony jhani ,near PC hotel abbottabad"
+        }
+
+    }
+        return addressDetails
+    }
+}
