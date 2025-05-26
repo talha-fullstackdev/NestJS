a60b9d8d6b67d1a47543c4adf8c083d6103c3def
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-26 17:11:42 +0500

    disable prettier for student module folder

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
