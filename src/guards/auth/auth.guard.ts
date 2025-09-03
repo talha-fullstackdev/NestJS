55496bfa06571677aeed657537962e3dba6f7513
talha-fullstackdev <talhafullstack786@gmail.com>
2025-09-03 15:26:50 +0500

    learn How to Protect Routes using Guards in Nest JS

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization']
+    return authHeader === "Bearer my-secret-token";
+  }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { Controller,Get,Param} from '@nestjs/common';
+import { Controller,Get,Param,UseGuards} from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
 @Controller('products')
 export class ProductsController {
     constructor( private readonly productsService : ProductsService ){}
         @Get() // hit when route is only products
+        @UseGuards(AuthGuard)
         getProducts(){
            return this .productsService.getAllProducts()
         }
